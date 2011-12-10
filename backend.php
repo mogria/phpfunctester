c33240f53bd3d5cc4a74adb345a76aa45ff2dee7
Mogria <m0gr14@gmail.com>
2011-12-10 16:38:40 +0100

    you can now select a type and the value will be casted to this type before its passed to the function

--- backend.php
+++ backend.php
@@ -1,23 +1,67 @@
 <?php
 
+function cast($var, $type) {
+  switch($type) {
+    default: 
+    case "string":
+      return (string) $var;
+      break;
+    case "int":
+      return (int) $var;
+      break;
+    case "bool":
+      return (bool) $var;
+      break;
+    case "float":
+      return (float) $var;
+      break;
+    case "double":
+      return (double) $var;
+      break;
+  }
+}
 
+
+// Is a function given?
 if(isset($_POST['func'])) {
   $func = $_POST['func'];
+  // does the function exist?
   if(function_exists($func)) {
-    $arg = isset($_POST['arg']) ? $_POST['arg'] : array();
-    $func = $_POST['func'];
 
-    $reflection = new ReflectionFunction($func);
-    $num_params = $reflection->getNumberOfRequiredParameters();
-    
-    if(count($arg) >= $num_params) {
-      ob_start();
-      var_dump(call_user_func_array($_POST['func'], $arg));
-      $content = ob_get_contents();
-      ob_end_clean();
-      echo htmlspecialchars($content);
+    // get all the arguments and the datatypes given
+    $arg = isset($_POST['arg']) && is_array($_POST['arg']) ? array_values($_POST['arg']) : array();
+    $type = isset($_POST['type']) && is_array($_POST['type']) ? array_values($_POST['type']) : array();
+
+    // the same amount?
+    if(count($arg) === count($type)) {
+
+      // cast the arguments to the given datatypes
+      foreach($arg as $key => $value) {
+        $arg[$key] = cast($value, $type[$key]);
+      }
+
+      // get number of params the function has
+      $reflection = new ReflectionFunction($func);
+      $num_params = $reflection->getNumberOfRequiredParameters();
+      
+      // enougth params given?
+      if(count($arg) >= $num_params) {
+        // write to a buffer
+        ob_start();
+
+        //call the function
+        var_dump(call_user_func_array($_POST['func'], $arg));
+
+        //get the content of the buffer
+        $content = ob_get_contents();
+        //clean the buffer
+        ob_end_clean();
+        echo htmlspecialchars($content);
+      } else {
+        echo "to few parameters given.";
+      }
     } else {
-      echo "to few parameters given.";
+      echo "Diffrent number of datatypes and parameters?!";
     }
   } else {
     echo "this function does not exist!";
