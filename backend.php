35f441ecc013cbfa603e2e5f3f32f1ae97b59f64
Mogria <m0gr14@gmail.com>
2011-12-10 16:06:38 +0100

    no more an php error if you pass too few arguments to a function

--- backend.php
+++ backend.php
@@ -1,11 +1,25 @@
 <?php
-if(function_exists($_POST['func'])) {
-  ob_start();
-  var_dump(call_user_func_array($_POST['func'], $_POST['arg']));
-  $content = ob_get_contents();
-  ob_end_clean();
 
-  echo htmlspecialchars($content);
-} else {
-  echo "this function does not exist!";
+
+if(isset($_POST['func'])) {
+  $func = $_POST['func'];
+  if(function_exists($func)) {
+    $arg = isset($_POST['arg']) ? $_POST['arg'] : array();
+    $func = $_POST['func'];
+
+    $reflection = new ReflectionFunction($func);
+    $num_params = $reflection->getNumberOfRequiredParameters();
+    
+    if(count($arg) >= $num_params) {
+      ob_start();
+      var_dump(call_user_func_array($_POST['func'], $arg));
+      $content = ob_get_contents();
+      ob_end_clean();
+      echo htmlspecialchars($content);
+    } else {
+      echo "to few parameters given.";
+    }
+  } else {
+    echo "this function does not exist!";
+  }
 }
